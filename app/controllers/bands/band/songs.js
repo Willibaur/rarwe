f488efad57b3f699cc28d726664fbf227d45a681
willibaur <williambautista@gmail.com>
2018-04-21 13:31:21 +0100

    Add disable functionality to Add buttons

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,12 +1,20 @@
+import { computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  title: '',
+
+  isAddButtonDisabled: computed('title', function() {
+    return isEmpty(this.get('title'));
+  }),
+
   actions: {
     updateRating: function(params) {
       var song = params.item;
       var rating = params.rating;
 
       song.set('rating', rating);
-    },    
+    },
   }
 });
