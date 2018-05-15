2b4ebb826d24c716e48d90ee03ae442f45358832
willibaur <williambautista@gmail.com>
2018-05-15 08:23:44 +0100

    Add alert when leaving edit details before saving

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,7 +1,20 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model: function() {
-    return this.modelFor('bands.band')
+  actions: {
+    willTransition(transition) {
+      var controller = this.get('controller');
+      var leave;
+
+      if (controller.get('isEditing')) {
+        leave = window.confirm("You have unsaved changes. Are you sure you want to leave?");
+
+        if (leave) {
+          controller.set('isEditing', false);
+        } else {
+          transition.abort();
+        }
+      }
+    },
   }
 });
